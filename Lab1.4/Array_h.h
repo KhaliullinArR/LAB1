8a79f8ed888482e6b0037d18b6b44ed8a241d924
Khlaiullin Artem <halli-a@mail.ru>
2021-10-18 23:46:21 +0300

    add Array_h.h

---
+++ Lab1.4/Array_h.h
@@ -0,0 +1,8 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+double* ArrayInput(int*, char*);
+int AmountLessThan(double*, int, double);
+int Output(double*, double*, double*, int, int, int, int*, int*, int*, double*);
+
+#endif
